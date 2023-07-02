01fc3c2bff12a75c2ab66afe42b57950e15bad98
Tanuj Dargan <dargantanuj@gmail.com>
2023-07-02 18:02:26 +0530

    updated gps code

--- Project_RiverWatch/OCR-AC-TD/ocr+image_classification.py
+++ Project_RiverWatch/OCR-AC-TD/ocr+image_classification.py
@@ -5,6 +5,8 @@ import pandas as pd
 from keras.models import load_model
 from paddleocr import PaddleOCR, draw_ocr
 from PIL import Image, ImageFont
+from GPSPhoto import gpsphoto
+import folium
 
 # Load the image classification model
 model = load_model('C:/users/tanuj/desktop/makers asylum/keras_model.h5')
@@ -16,40 +18,26 @@ class_names = [class_name.strip() for class_name in open('C:/users/tanuj/desktop
 ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
 # Define the folder path
-folder_path = "C:/users/tanuj/desktop/makers asylum/test_set/"
+folder_path = "C:/users/tanuj/desktop/makers asylum/FINAL_TEST/"
 
-def extraction_exif():
+def extraction_exif(image_list):
+        data_location = []
+        for a in image_list:
+            b = gpsphoto.getGPSData(os.path.join(os.getcwd(), a))
+            if b is not None and 'Latitude' in b and 'Longitude' in b:
+                data_location.append([b['Latitude'], b['Longitude']])
 
-    print(image_list)
-    import pandas as pd
-    from GPSPhoto import gpsphoto
+        if data_location:
+            df = pd.DataFrame(data=data_location, index=image_list, columns=['Latitude', 'Longitude'])
+            df.to_csv("./coordinates.csv")
+        else:
+            df = pd.DataFrame(columns=['Latitude', 'Longitude'])
 
-    data_location = []
-    for a in image_list:
-    b = gpsphoto.getGPSData(os.getcwd() + f'\\{a}')
-    data_location.append(b)
-        
-    df = pd.DataFrame(data = data_location, index = image_list)
-    print(df.head())
-    df.to_csv("C:\\Users\\Praneeth\\Desktop\\GPS\\2nd April 2023\\020423_GPSdata.csv")
-
-    sw = (df.Latitude.max() + 3, df.Longitude.min() - 3)
-    ne = (df.Latitude.min() - 3, df.Longitude.max() + 3)
-
-    import folium
-    m = folium.Map()
-
-    for lat, lon, in zip(df.Latitude.values, df.Longitude.values,):
-        folium.CircleMarker(
-            [lat, lon], 
-            ).add_to(m)
-
-    m.fit_bounds([sw, ne])
-    display(m)
-
+        return df
 
 def classify_images_with_ocr(image_folder):
     data = []
+    image_list = []
     for file_name in os.listdir(image_folder):
         if file_name.endswith('.jpg') or file_name.endswith('.png'):
             path = os.path.join(image_folder, file_name)
@@ -80,7 +68,7 @@ def classify_images_with_ocr(image_folder):
             scores = [line[1][1] for line in result]
 
             # Draw OCR text on the image
-            im_show = draw_ocr(img_rgb, boxes, txts, scores, font_path='C:/Users/tanuj/Desktop/Makers ASylum/Times-New-Roman/times new roman.ttf')
+            im_show = draw_ocr(img_rgb, boxes, txts, scores, font_path='./times new roman.ttf')
             im_show = Image.fromarray(im_show)
 
             # Save the result image with the updated filename
@@ -88,18 +76,40 @@ def classify_images_with_ocr(image_folder):
             output_filepath = os.path.join(image_folder, output_filename)
             im_show.save(output_filepath)
 
+            image_list.append(file_name)
+
             # Store the image path, predicted class, confidence score, and OCR text in a dictionary
             data.append({
                 'Image': path,
                 'Class': class_name,
                 'Confidence': confidence,
-                'OCR Text': ocr_text,
-                'Latitude': df.Latitude,
-                'Longitude': df.Longitude
+                'OCR Text': ocr_text
             })
 
     # Create a dataframe from the collected data
     df = pd.DataFrame(data)
+
+    # Call the extraction_exif function to get GPS coordinates
+    gps_df = extraction_exif(image_list)
+
+    # Merge the classification results with the GPS coordinates based on the image filenames
+    df = pd.merge(df, gps_df, left_on='Image', right_index=True, how='left')
+
+    # Save the GPS coordinates to a CSV file
+    gps_df.to_csv("./coordinates.csv")
+
+    if not gps_df.empty:
+        # Create a folium map and plot the coordinates
+        sw = (gps_df['Latitude'].max() + 3, gps_df['Longitude'].min() - 3)
+        ne = (gps_df['Latitude'].min() - 3, gps_df['Longitude'].max() + 3)
+        m = folium.Map()
+        for lat, lon in zip(gps_df['Latitude'].values, gps_df['Longitude'].values):
+            if not pd.isnull(lat) and not pd.isnull(lon):
+                folium.CircleMarker([lat, lon]).add_to(m)
+        m.fit_bounds([sw, ne])
+        m.save("map.html")
+
+    # Return the dataframe with classification results
     return df
 
 result_df = classify_images_with_ocr(folder_path)
@@ -110,7 +120,7 @@ print(result_df)
 # Check if the classification_results.csv file exists and has columns
 if not os.path.isfile('classification_results.csv') or os.stat('classification_results.csv').st_size == 0:
     # If the file does not exist or is empty, create a new empty CSV file with column headers
-    empty_df = pd.DataFrame(columns=['Image', 'Class', 'Confidence', 'OCR Text'])
+    empty_df = pd.DataFrame(columns=['Image', 'Class', 'Confidence', 'OCR Text', 'Latitude', 'Longitude'])
     empty_df.to_csv('classification_results.csv', index=False)
 
 # Load the existing classification results CSV
